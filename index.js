3e3a7ec38b2463cb5977a9f4356db2658e460024
choijeehyuk <jeehyuk97@daum.net>
2022-12-07 20:22:02 +0900

    write csv

--- index.js
+++ index.js
@@ -1,34 +1,66 @@
-const xlsx = require("xlsx");
+const fs = require("fs");
 
-const axios = require("axios"); // for ajax
-const cheerio = require("cheerio"); // for html parsing
+const { parse } = require("csv-parse");
+const { stringify } = require("csv-stringify/sync");
 
-const workbook = xlsx.readFile("xlsx/data.xlsx");
-const ws = workbook.Sheets.영화목록;
-const records = xlsx.utils.sheet_to_json(ws);
+const csv = fs.readFileSync("csv/data.csv").toString("utf-8");
+const parser = parse({ delimiter: "," });
+const records = [];
 
-for (const [i, v] of records.entries()) {
-}
+parser.on("readable", function () {
+  let record;
+  while ((record = parser.read()) !== null) {
+    records.push(record);
+  }
+});
+parser.on("error", function (err) {
+  console.error(err.message);
+});
+parser.on("end", function () {
+  console.log("end");
+});
+
+parser.write(csv);
+parser.end();
+
+const puppeteer = require("puppeteer");
 
 const crawler = async () => {
-  const test = records.map(async (r, i) => {
-    const res = await axios.get(r.링크);
-    console.log(i);
-    if (res.status === 200) {
-      const html = res.data;
-      const $ = cheerio.load(html);
-      const score = $(".score.score_left .star_score").text().trim();
-      console.log(r.제목, "평점 : ", score);
-      return score;
-    }
-    return 3;
-  });
-  console.log(test);
+  try {
+    const result = [];
+    const browser = await puppeteer.launch({ headless: true });
+
+    await Promise.all(
+      records.map(async (v, i) => {
+        try {
+          const page = await browser.newPage();
+          await page.goto(v[1]);
+          const scoreEl = await page.$(".score.score_left .star_score");
+
+          if (scoreEl) {
+            const text = await page.evaluate((tag) => tag.textContent, scoreEl);
+            console.log(text.trim());
+            //   result.push([v[0], v[1], text.trim()]);
+            result[i] = [v[0], v[1], text.trim()];
+          }
+
+          await page.waitForTimeout(3000);
+          await page.close();
+        } catch (err) {
+          console.error(err);
+        }
+      })
+    );
+
+    await browser.close();
+
+    console.log(result);
+    const str = stringify(result);
+    console.log(str);
+    fs.writeFileSync("csv/result.csv", str);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 crawler();
-
-// const ret = [1, 2, 3, 4, 5].map((r, i) => {
-//   console.log(r);
-// });
-// console.log(ret);
