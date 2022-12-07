d5a2b4d76c41cade849d60d1181ca0f16597be78
choijeehyuk <jeehyuk97@daum.net>
2022-12-07 14:54:11 +0900

    csv

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("haha");
+const { parse } = require("csv-parse");
+
+const fs = require("fs");
+
+const csv = fs.readFileSync("csv/data.csv");
+
+const records = [];
+const parser = parse({
+  delimiter: ",",
+});
+
+parser.on("readable", () => {
+  let record;
+  while ((record = parser.read()) !== null) {
+    records.push(record);
+  }
+});
+
+parser.on("end", function () {
+  console.log("end");
+});
+
+parser.on("error", function (err) {
+  console.error(err.message);
+});
+
+parser.write(csv.toString());
+parser.end();
+
+console.log(records);
