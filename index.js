d67904d8b3444f1cfd4cf406bed97d5531700ce0
choijeehyuk <jeehyuk97@daum.net>
2022-12-08 15:58:05 +0900

    unsplash page crawl

--- index.js
+++ index.js
@@ -1,101 +1,58 @@
 const fs = require("fs");
 const axios = require("axios");
-
-const { parse } = require("csv-parse");
-const { stringify } = require("csv-stringify/sync");
-
-const csv = fs.readFileSync("csv/data.csv").toString("utf-8");
-const parser = parse({ delimiter: "," });
-const records = [];
-
-fs.readdir("screenshot", (err) => {
-  if (err) {
-    console.error("screenshot 폴더 생성");
-    fs.mkdirSync("screenshot");
-  }
-});
-
-fs.readdir("poster", (err) => {
-  if (err) {
-    console.error("poster 폴더 생성");
-    fs.mkdirSync("poster");
-  }
-});
-
-parser.on("readable", function () {
-  let record;
-  while ((record = parser.read()) !== null) {
-    records.push(record);
-  }
-});
-parser.on("error", function (err) {
-  console.error(err.message);
-});
-parser.on("end", function () {
-  console.log("end");
-});
-
-parser.write(csv);
-parser.end();
-
 const puppeteer = require("puppeteer");
+
+fs.readdir("unsplash", (err) => {
+  if (err) {
+    console.error("unsplash 폴더 생성");
+    fs.mkdirSync("unsplash");
+  }
+});
 
 const crawler = async () => {
   try {
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: ["--window-size=1440,900"],
-    });
-
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
-    page.setViewport({
-      width: 1440,
-      height: 900,
-    });
+    await page.goto(`https://unsplash.com`);
 
-    await page.setUserAgent(
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
-    );
-
-    console.log(await page.evaluate("navigator.userAgent"));
-
-    for (const [i, v] of records.entries()) {
-      await page.goto(v[1]);
-
-      const result = await page.evaluate(() => {
-        const scoreEl = document.querySelector(".score.score_left .star_score");
-        let score = "";
-        if (scoreEl) {
-          score = scoreEl.textContent.trim();
+    let imgs = [];
+    while (imgs.length < 100) {
+      const result = await page.evaluate(async () => {
+        window.scrollTo(0, 0);
+        const div = document.querySelectorAll("figure");
+        const imgs = [];
+        console.log(div.length);
+        console.log(div);
+        if (div.length > 0) {
+          div.forEach((v) => {
+            const src = v.querySelector("img.YVj9w").src;
+            if (src) {
+              imgs.push(src);
+              v.parentElement.removeChild(v);
+            }
+          });
         }
+        window.scrollBy(0, 200);
 
-        const imgEl = document.querySelector(".poster img");
-        let img = "";
-        if (imgEl) {
-          img = imgEl.src;
-        }
-        return { score, img };
+        return imgs;
       });
-
-      if (result.img) {
-        await page.screenshot({
-          path: `screenshot/${v[0]}.png`,
-          //   fullPage: true,
-          clip: {
-            x: 100,
-            y: 100,
-            width: 300,
-            height: 300,
-          },
-        });
-        const imgResult = await axios.get(result.img.replace(/\?.*$/, ""), {
-          responseType: "arraybuffer",
-        });
-        fs.writeFileSync(`poster/${v[0]}.jpg`, imgResult.data);
-      }
+      await page.waitForSelector("figure img.YVj9w", { timeout: 3000 });
+      imgs = imgs.concat(result);
+      console.log(imgs.length);
     }
-    await page.close();
-    await browser.close();
+
+    const set = new Set(imgs);
+    set.forEach(async (link) => {
+      console.log(link);
+      const imgBuffer = await axios(link, { responseType: "arraybuffer" });
+      fs.writeFileSync(`unsplash/${new Date().valueOf()}.jpeg`, imgBuffer.data);
+    });
+    console.log("set : ", set.size);
+    console.log("img : ", imgs.length);
+    console.log("complete loading!!");
+
+    // await page.close();
+    // await browser.close();
   } catch (e) {
     console.error(e);
   }
